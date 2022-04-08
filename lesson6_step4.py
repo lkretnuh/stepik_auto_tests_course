3dda888c636326a5b4f4b5847b14217e7a1971ba
lkretnuh <103199592+lkretnuh@users.noreply.github.com>
2022-04-08 16:25:51 +0300

    simple test add
    homework from previous module

---
+++ lesson6_step4.py
@@ -0,0 +1,40 @@
+from re import VERBOSE
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver 
+import time
+import math
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+link = "http://suninjuly.github.io/explicit_wait2.html"
+
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    button = browser.find_element_by_css_selector('#book')
+    price = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#price"), '$100')
+    )
+    button.click()
+    x_element = browser.find_element_by_css_selector("#input_value")
+    x = x_element.text
+    y = calc(x)
+    input1 = browser.find_element_by_css_selector('#answer')
+    input1.send_keys(y)
+    button_submit2 = browser.find_element_by_css_selector('[type="submit"]')
+    button_submit2.click()
+
+
+
+
+
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
